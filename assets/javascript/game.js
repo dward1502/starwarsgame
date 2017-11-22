02ab13f8ed6d67b138978a7251a02b592d51bec3
dward1502@gmail.com <dward1502@gmail.com>
2017-11-22 10:11:35 -0800

    starwarsgame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,55 @@
-//object for the characters
-var game = {
-     characters: {
-        chewie: {
-            hp : 100,
-            attPow : 20,
-            countAtt : 5,
-            img : '../images/chewbacca.jpg'
+//Initializes javascript after HTML page is completed, allows for the declaring JQuery and javascript script at the top of the HTML page
+$(document).ready(function(){
+//Global Variables inside the ready function
+/*Can add theme song here */
+var enemyChosen = false;
+var defeated = false;
+var character;
+var characterChosen;
+var opponent;
+/* initialize theme song here look up how to do this*/
+
+//Object character declarations
+var characters =[ 
+        chewie = {
+            name : "Chewbacca",
+            img : "<img src = 'assets/images/chewbacca.jpg' id = 'chewie'>",
+            baseAttack : 4,
+            attack : 4,
+            health : 80,
+            /*add sound*/
         }
-        
-    };
-}
+        chewie = {
+            name: "Chewbacca",
+            img: "<img src = 'assets/images/chewbacca.jpg' id = 'chewie'>",
+            baseAttack: 4,
+            attack: 4,
+            health: 80,
+        /*add sound*/
+        }
+        chewie = {
+        name: "Chewbacca",
+        img: "<img src = 'assets/images/chewbacca.jpg' id = 'chewie'>",
+        baseAttack: 4,
+        attack: 4,
+        health: 80,
+        /*add sound*/
+    }
+      chewie = {
+        name: "Chewbacca",
+        img: "<img src = 'assets/images/chewbacca.jpg' id = 'chewie'>",
+        baseAttack: 4,
+        attack: 4,
+        health: 80,
+        /*add sound*/
+    }
+
+]
+
+
+
+
+
+
+});
+
