62d75fc53fdb01085e2e33277f8d2d0f1eb5e9c6
Daniel Ward <dward1502@gmail.com>
2017-11-21 23:20:22 -0800

    starwarsgame

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//object for the characters
+var game = {
+     characters: {
+        chewie: {
+            hp : 100,
+            attPow : 20,
+            countAtt : 5,
+            img : '../images/chewbacca.jpg'
+        }
+        
+    };
+}
